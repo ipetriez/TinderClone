5bfcacffa1686cbe85658627fe10a7a242b7fdcc
Sergey Petrosyan <ipetriez@icloud.com>
2025-10-27 14:09:18 +0400

    Partially implement swiping logic in CardView.

--- TinderClone/Views/CardView.swift
+++ TinderClone/Views/CardView.swift
@@ -8,14 +8,8 @@
 import SwiftUI
 
 struct CardView: View {
-    
-    private var cardWidth: CGFloat {
-        UIScreen.main.bounds.width - 20
-    }
-    
-    private var cardHeight: CGFloat {
-        UIScreen.main.bounds.height / 1.45
-    }
+    @State private var xOffset: CGFloat = 0
+    @State private var degrees: Double = 0
     
     var body: some View {
         ZStack(alignment: .bottom) {
@@ -29,6 +23,46 @@ struct CardView: View {
                 .frame(width: cardWidth)
                 .padding(.horizontal)
         }
+        .offset(x: xOffset)
+        .rotationEffect(.degrees(degrees))
+        .animation(.snappy(), value: xOffset)
+        .gesture(
+            DragGesture()
+                .onChanged({ value in
+                    onDragChanged(value)
+                })
+                .onEnded({ value in
+                    onDragEnded(value)
+                }))
+    }
+    
+    private func onDragChanged(_ value: DragGesture.Value) {
+        xOffset = value.translation.width
+        degrees = Double(value.translation.width / 25)
+    }
+    
+    private func onDragEnded(_ value: DragGesture.Value) {
+        let width = value.translation.width
+        if abs(width) <= abs(screenCutOff) {
+            xOffset = 0
+            degrees = 0
+        } else {
+            
+        }
+    }
+}
+
+private extension CardView {
+    var cardWidth: CGFloat {
+        UIScreen.main.bounds.width - 20
+    }
+    
+    var cardHeight: CGFloat {
+        UIScreen.main.bounds.height / 1.45
+    }
+    
+    var screenCutOff: CGFloat {
+        (UIScreen.main.bounds.width / 2) * 0.8
     }
 }
 
