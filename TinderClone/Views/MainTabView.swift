f459b05ebfdeef575d8804c4d797a9856666a903
Sergey Petrosyan <ipetriez@icloud.com>
2025-10-27 13:03:39 +0400

    Create basic TabView.

---
+++ TinderClone/Views/MainTabView.swift
@@ -0,0 +1,18 @@
+//
+//  MainTabView.swift
+//  TinderClone
+//
+//  Created by Sergey Petrosyan on 27.10.25.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainTabView()
+}
